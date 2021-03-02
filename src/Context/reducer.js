6b0c9d46ec6e4bf244782e61efdf6a6565beebc4
afzalMa <afzal.m@etvbharat.com>
2021-03-02 15:51:12 +0530

    added the project code

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import {ADD_TODO, REMOVE_TODO} from "./action.types"
 
-export default  (state, action) => {
-  switch (action) {
+export default (state, action) => {
+  switch (action.type) {
     case ADD_TODO:
       return[...state, action.payload];
     case REMOVE_TODO:
-      return state.filter(todo => todo.id !== action.paload)
+      return state.filter(todo => todo.id !== action.payload)
     default:
     return state;
   }
